57eacd70e686c52dc609f806815fc68a34e0c808
dan <daan.forever@gmail.com>
2012-03-12 21:14:08 +0400

    simple url fetch is work!

--- bin/crawler.rb
+++ bin/crawler.rb
@@ -1,5 +1,17 @@
 #!/usr/bin/env ruby
 
-require '../lib/web-crawler.rb'
+require './lib/web-crawler.rb'
 
-WebCrawler.hi();
+# Algorithm:
+#  ├┬ Check for last session
+#  │└─ If none session available init with http://dron.me/
+#  ├─ slkej
+#  ├─ sdfsd
+#  └─ dfasd
+
+crawler = WebCrawler.new();
+
+crawler.init('http://dron.me') unless crawler.session();
+crawler.step();
+crawler.dump();
+
--- lib/web-crawler.rb
+++ lib/web-crawler.rb
@@ -1,7 +1,42 @@
 
+require 'rubygems'
+require 'httparty'
+require 'nokogiri'
+#require 'uri'
+
 class WebCrawler
+
     @crawler_version = "Web Crawler version 0.0.1";
+
     def self.version
         @crawler_version;
     end
+
+    def initialization
+    end
+
+    def init(url)
+        self.get(url)
+    end
+
+    def get(url)
+        r = HTTParty.get(url).body;
+        r = Nokogiri.parse(r);
+        hash = Hash.new
+        r.search('a').each do |a|
+            url = a['href'].match(/http:\/\/([^\/]+)/)[1] 
+            hash[url] = 1
+        end
+        hash
+    end
+
+    def dump
+    end
+
+    def session
+    end
+
+    def step
+    end
 end
+
