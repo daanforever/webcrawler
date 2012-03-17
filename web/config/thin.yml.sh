b7b4f17267b169c5ed4041c994ae0460ec8f778c
dan <daan.forever@gmail.com>
2012-03-18 03:10:35 +0400

    interim

--- web/config/thin.yml.sh
+++ web/config/thin.yml.sh
@@ -1,1 +1,1 @@
-thin -d -u www-data -g www-data -s 2 -e development -l /var/log/thin/thin.log -P /var/run/thin.pid -a 127.0.0.1 -p 3003 --debug --trace -C webcrawler.yml config
+thin -d -u www-data -g www-data -s 2 -e development -l /var/log/thin/thin.log -P /var/run/thin.pid -a 127.0.0.1 -p 3003 --debug --trace -A rails -C thin.yml config
