5812e0782a2398d6300a081c982850eb6f379fec
dan <daan.forever@gmail.com>
2012-03-18 18:57:23 +0400

    v0.0.5 separate crawler and webinterface

--- web/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Web::Application.config.secret_token = 'c45ea663becfd486bbfa30e9bf4f2f02e510f72bfa198daa9e4c218b3681a283d081af059cef6cb39387fa325246eb8953f2fd64ca009e17875c22bac2a2c901'
